241f4bee18372813ba36eef969dd99b14499ed67
ClauHaus <claudio.aime32@gmail.com>
2021-09-25 21:07:42 +0200

    New update

--- src/Contact.js
+++ src/Contact.js
@@ -41,50 +41,58 @@ const Contact = () => {
   //     .catch((error) => formInformation.error);
   // };
 
-  // const [playClose] = useSound(close, { volume: 0.5 });
+  const [playClose] = useSound(close, { volume: 0.5 });
 
-  // const closeSound = () => {
-  //   closeContact();
-  //   playClose();
-  //   setFooter("");
-  // };
+  const closeSound = () => {
+    closeContact();
+    playClose();
+    setFooter("");
+  };
 
   return (
-    <form action="POST" data-netlify="true">
-      <div class="fields">
-        <div class="field half">
-          <input type="text" name="name" id="name" placeholder="Name" />
-        </div>
-        <div class="field half">
-          <input type="email" name="email" id="email" placeholder="Email" />
-        </div>
-        <div class="field">
-          <textarea
-            name="message"
-            id="message"
-            placeholder="Message"
-            rows="7"
-          ></textarea>
-        </div>
-        <div class="field">
-          <input
-            type="file"
-            name="myfile"
-            id="myfile"
-            placeholder="Upload File"
-            rows="7"
-          ></input>
-        </div>
-        <div class="filed">
-          <div data-netlify-recaptcha="true"></div>
-        </div>
-      </div>
-      <ul class="actions">
-        <li>
-          <input type="submit" value="Send Message" class="button primary" />
-        </li>
-      </ul>
-    </form>
+    <>
+      <section id="contact" className={`section-contact ${closingContact}`}>
+        <form action="POST" data-netlify="true">
+          <div class="fields">
+            <div class="field half">
+              <input type="text" name="name" id="name" placeholder="Name" />
+            </div>
+            <div class="field half">
+              <input type="email" name="email" id="email" placeholder="Email" />
+            </div>
+            <div class="field">
+              <textarea
+                name="message"
+                id="message"
+                placeholder="Message"
+                rows="7"
+              ></textarea>
+            </div>
+            <div class="field">
+              <input
+                type="file"
+                name="myfile"
+                id="myfile"
+                placeholder="Upload File"
+                rows="7"
+              ></input>
+            </div>
+            <div class="filed">
+              <div data-netlify-recaptcha="true"></div>
+            </div>
+          </div>
+          <ul class="actions">
+            <li>
+              <input
+                type="submit"
+                value="Send Message"
+                class="button primary"
+              />
+            </li>
+          </ul>
+        </form>
+      </section>
+    </>
   );
 };
 
